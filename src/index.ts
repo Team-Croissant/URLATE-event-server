6b982a4134fb051132a2eac46dc1b27c74628285
HyeokjinKang <coupy@croissant.tech>
2021-12-09 11:34:21 +0000

    소켓을 구버전으로 전환

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ const io = require("socket.io")(config.project.port);
 
 io.on("connection", (socket) => {
   console.log(`${socket.id} : User Connected.`);
-  // redisClient.set(`socket${socket.id}`, socket.handshake.query.id);
-  // redisClient.set(`user${socket.id}`, socket.handshake.query.name);
+  if (socket.handshake.query.id != 0) {
+    io.emit("connected", socket.handshake.query.id, socket.id);
+  }
 
   socket.on("disconnect", () => {
     console.log(`${socket.id} : User Disconnected.`);
+    io.emit("disconnected", socket.id);
   });
 });
