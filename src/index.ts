58c6c32e4ae01d1c3c4220d8ffefc6c8425a7345
HyeokjinKang <coupy@croissant.tech>
2021-12-09 15:46:04 +0000

    소켓연결을 더 확실히 하고, 연결 끊김등 예외처리를 포함합니다.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 const config = require(__dirname + "/../config/config.json");
 const io = require("socket.io")(config.project.port);
+let adminId = "";
 
 io.on("connection", (socket) => {
   console.log(`${socket.id} : User Connected.`);
-  if (socket.handshake.query.id != 0) {
-    io.emit("connected", socket.handshake.query.id, socket.id);
-  }
+  io.to(socket.id).emit("broadcast", socket.id);
+
+  socket.on("admin", () => {
+    adminId = socket.id;
+  });
+
+  socket.on("handshake", (id, socketId) => {
+    io.emit("handshake", id, socketId);
+  });
+
+  socket.on("connected", (socketId) => {
+    io.to(socketId).emit("connected");
+  });
 
   socket.on("disconnect", () => {
     console.log(`${socket.id} : User Disconnected.`);
-    io.emit("disconnected", socket.id);
+    if (socket.id == adminId) {
+      io.emit("admin disconnected");
+    } else {
+      io.emit("disconnected", socket.id);
+    }
+  });
+
+  socket.on("initialize", (socketId) => {
+    io.to(socketId).emit("initialize");
   });
 });
